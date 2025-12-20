a1546915f29e0cb3f5f386564cd4fd04f69800b0
Santeri Kuusela <santeriviljokuusela@gmail.com>
2025-12-20 16:45:11 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
 // Valitse yksi:
-import Board from "./component/Board"; // ORIGINAALI
-//import Board from "./component/front" // LEEVIN
+//import Board from "./component/Board"; // ORIGINAALI
+import Board from "./component/front"; // LEEVIN
 
 function App() {
   return (
