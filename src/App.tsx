c857c547f32f959a8f332cd2c6419fbe5b06186a
sirQuillotine <santeriviljokuusela@gmail.com>
2025-12-28 23:03:01 +0200

    Pohjaa mobiiliversiolle

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,25 @@ import {
   Navigate,
 } from "react-router-dom";
 import Sanapeli from "./Sanapeli";
+import SanapeliMobile from "./SanaPeliMobile";
 
 const SeedRedirect = () => {
   const { seed } = useParams();
   return <Navigate to="/" state={{ seed }} replace />;
 };
 
+function isMobile() {
+  return window.innerWidth <= 800 && window.innerHeight <= 600;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Sanapeli />} />
+        <Route
+          path="/"
+          element={isMobile() ? <SanapeliMobile /> : <Sanapeli />}
+        />
         <Route path="/:seed" element={<SeedRedirect />} />
       </Routes>
     </BrowserRouter>
